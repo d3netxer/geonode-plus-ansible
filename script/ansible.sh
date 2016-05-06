3634c319543a61a7eaa27eadc928841cb18e8127
d3netxer <tommygun320@yahoo.com>
2016-05-06 00:39:38 -0400

    adding play

--- script/ansible.sh
+++ script/ansible.sh
@@ -1,8 +1,9 @@
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update -y
 sudo apt-get install ansible -y
-sudo mv /etc/ansible/hosts /etc/ansible/hosts.orig
-sudo touch /etc/ansible/hosts
-sudo chmod -R 777 /etc/ansible/hosts
-sudo echo "[local]">>/etc/ansible/hosts
-sudo echo "localhost              ansible_connection=local">>/etc/ansible/hosts
+sudo ansible-galaxy install d3netxer.geonode-plus-ansible
+#sudo mv /etc/ansible/hosts /etc/ansible/hosts.orig
+#sudo touch /etc/ansible/hosts
+#sudo chmod -R 777 /etc/ansible/hosts
+#sudo echo "[local]">>/etc/ansible/hosts
+#sudo echo "localhost              ansible_connection=local">>/etc/ansible/hosts
